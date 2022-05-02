6eddd7ccc1c9466c0a684bfbd3d953491fcf7012
luismeza8 <angel841bv@gmail.com>
2022-05-02 15:26:27 -0700

    Correccion de errores

---
+++ BCD.py
@@ -0,0 +1,14 @@
+# def comprobar_numero(valor):
+#     if "0" not in valor or "1" not in valor:
+#         return True
+
+#     return False
+
+# def separar_numero(valor, )
+
+def bcd_a_decimal(numero):
+    pass
+
+
+def decimal_a_bcd(numero):
+    pass
